3b4a25be8e9c4191e3783f4e91c9381400185639
Andrey Petrov <19graff91@gmail.com>
2016-04-10 04:42:35 -0400

    Refactoring.

---
+++ SP/SP.Shell/ViewModel/DiscreteCriteriaRangeViewModel.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using GalaSoft.MvvmLight;
+
+using SP.Shell.Models;
+
+namespace SP.Shell.ViewModel
+{
+    public class DiscreteCriteriaRangeViewModel : ViewModelBase, ICriteriaRangeSelector
+    {
+        private readonly Action rangeChangedAction;
+        private readonly Func<IEnumerable<string>> criteriaSelector;
+        private readonly Func<RecordsCollection> collectionSelector;
+
+        public DiscreteCriteriaRangeViewModel(
+            Action rangeChangedAction,
+            Func<IEnumerable<string>> criteriaSelector,
+            Func<RecordsCollection> collectionSelector)
+        {
+            this.rangeChangedAction = rangeChangedAction;
+            this.criteriaSelector = criteriaSelector;
+            this.collectionSelector = collectionSelector;
+
+            UpdateProperties();
+        }
+
+        public IEnumerable<SelectableValue> Values { get; private set; }
+
+        public string SelectedValues
+        {
+            get
+            {
+                return Values == null
+                           ? string.Empty
+                           : string.Join(", ", Values.Where(v => v.Selected).Select(v => v.Value));
+            }
+        }
+
+        public void Apply()
+        {
+            collectionSelector().Apply(Values.Where(v => v.Selected).Select(v => v.Value).ToList());
+        }
+
+        public bool CanApply()
+        {
+            return !string.IsNullOrWhiteSpace(SelectedValues);
+        }
+
+        private void UpdateProperties()
+        {
+            Values =
+                criteriaSelector()
+                    .Distinct()
+                    .Where(v => !string.IsNullOrEmpty(v))
+                    .Select(v => new SelectableValue(v, true, SelectionOnChange))
+                    .ToList();
+
+            RaisePropertyChanged(() => Values);
+            RaisePropertyChanged(() => SelectedValues);
+        }
+
+        private void SelectionOnChange()
+        {
+            rangeChangedAction();
+            RaisePropertyChanged(() => SelectedValues);
+        }
+    }
+}
